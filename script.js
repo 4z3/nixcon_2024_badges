79ea6dbc5e920bd19c31ee159bf287cc21516e7c
panda <p4nd4@posteo.de>
2024-10-07 20:28:21 +0200

    remove cheerio package
    replace strings with builtin methods
    escape ‘\’ in name

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const fs = require('fs');
-const cheerio = require('cheerio');
-const data = JSON.parse(fs.readFileSync('example.json'));
 const svgTemplate = fs.readFileSync('badges_a7.svg', 'utf8');
+const data = JSON.parse(fs.readFileSync('example.json'));
 const outputDir = './output/';
 if (!fs.existsSync(outputDir)){
     fs.mkdirSync(outputDir);
 }
+const dummyName = "{NAME} ";
+const dummyPic = "../../../../Volumes/u267156.your-storagebox.de/illusUndArbeit/nix/nixcon_2024/avatar.jpg";
+
 const jsonEntries = data.entries;
 console.log('entries: ', jsonEntries);
 
 jsonEntries.forEach((entry) => {
   const {name, pictureUrl} = entry;
   console.log(`${name}`);
-  const $ = cheerio.load(svgTemplate, {xmlMode: false});
-  const nameElement = $('#name text');
-  const pictureUrlElement = $('image[width="386"][height="386"]');
 
-  // update <text> element
-  nameElement.text(name);
-  // update <image> element
-  pictureUrlElement.attr("href", pictureUrl);
+  const output =
+    svgTemplate
+      .replace(dummyName, name)
+      .replace(dummyPic, pictureUrl)
+      ;
 
-  // replace spaces and create filename
-  const fileName = `${name.replace(/\s+/g, '_')}.svg`;
+  const fileName = `${name.replace(/\//g, '_')}.svg`;
   const filePath = `${outputDir}${fileName}`;
 
-  fs.writeFileSync(filePath, $.xml());
-  console.log(`file created: ${filePath}`);
+  fs.writeFileSync(filePath, output);
 });
